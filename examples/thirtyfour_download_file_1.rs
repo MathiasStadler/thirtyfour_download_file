5ae286aaec9b8bd4eb7480991abed846cd61dcd6
Mathias <email@example.com>
2024-08-06 15:41:46 +0000

    save

---
+++ examples/thirtyfour_download_file_1.rs
@@ -0,0 +1,56 @@
+use std::error::Error;
+use std::process;
+use std::fmt;
+use std::io::Write;
+
+#[allow(unused_imports)]
+use log::{debug, error, info, log_enabled, Level};
+
+use std::env::set_var;
+
+#[allow(unused_imports)]
+use thirtyfour::ChromiumLikeCapabilities;
+#[allow(unused_imports)]
+use thirtyfour::{prelude::WebDriverError, By, DesiredCapabilities, Key, WebDriver, WebElement};
+
+#[allow(dead_code)]
+#[derive(Debug)]
+struct MyError(String);
+
+impl fmt::Display for MyError {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(f, "There is an error: {}", self.0)
+    }
+}
+
+impl Error for MyError {}
+
+fn main() -> Result<(), Box<dyn Error>> {
+    set_var("RUST_LOG", "debug");
+
+    env_logger::builder()
+        .format(|buf, record| {
+            let warn_style = buf.default_level_style(log::Level::Warn);
+            let _timestamp = buf.timestamp();
+            writeln!(
+                buf,
+                // FROM HERE
+                // https://docs.rs/env_logger/latest/src/custom_format/custom_format.rs.html#35
+                "{}:{}  {warn_style}{}{warn_style:#} - {}",
+                // record.level(),
+                record.file().unwrap_or("unknown"),
+                record.line().unwrap_or(0),
+                // chrono::Local::now().format("%Y-%m-%dT%H:%M:%S"),
+                record.level(),
+                record.args(),
+            )
+        })
+        .init();
+
+    error!("RUST_LOG maybe NOT enable");
+    error!("Used: => RUST_LOG=info < prg >");
+
+    info!("env_logger: ended");
+    process::exit(0);
+
+}
